4d9b4dfba55512710ad0033f230e8eab405bbd4b
alex <polovinkin.a.c@mail.ru>
2021-07-28 16:23:51 +0300

    calculator simple working version

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 function calculateCashback(specialCategoryPurchases, otherCategoryPurchases) {
         const specialCategoryPercent = 0.03;
         const otherCategoryPercent = 0.01;
@@ -17,4 +16,39 @@ function calculateCashback(specialCategoryPurchases, otherCategoryPurchases) {
 }
 const cashback = calculateCashback(5000, 10000);
 console.log(cashback);
+function handleClick(evt) {
+        evt.preventDefault(); // отменяем поведение по умолчанию
+
+        const specialAmountInputEl = document.getElementById('special-amount-input');
+        const specialAmount = Number(specialAmountInputEl.value);
+        if (Number.isNaN(specialAmount)){
+                // TODO: show error
+                return;
+        }
+        if (!Number.isFinite(specialAmount)){
+                // TODO: show error
+                return;
+        }
+
+        const otherAmountInputEl = document.getElementById('other-amount-input');
+        const otherAmount = Number(otherAmountInputEl.value);
+        if (Number.isNaN(otherAmount)){
+                // TODO: show error
+                return;
+        }
+        if (!Number.isFinite(otherAmount)){
+                //TODO: show error
+                return;
+        }
+
+        const result = calculateCashback(specialAmount, otherAmount);
+        const specialCashbackEl = document.getElementById('special-cashback');
+        specialCashbackEl.textContent = `${result.specialCategoryCashback} руб.`;
+        const otherCashbackEl = document.getElementById('other-cashback');
+        otherCashbackEl.textContent = `${result.otherCategoryCashback} руб.`;
+        const totalCashbackEl = document.getElementById('total-cashback');
+        totalCashbackEl.textContent = `${result.totalCashback} руб.`;
+}
+const formEl = document.getElementById('cashback-form');
+formEl.onclick = handleClick;
 
